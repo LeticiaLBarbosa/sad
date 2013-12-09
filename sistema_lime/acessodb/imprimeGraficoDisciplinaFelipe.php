52c10452af7c013a087e4bb43aea879c0bc3300f
Felipe Sales <sales.felipeb@gmail.com>
2013-12-09 13:21:49 -0300

    ageitando cookie

--- sistema_lime/acessodb/imprimeGraficoDisciplinaFelipe.php
+++ sistema_lime/acessodb/imprimeGraficoDisciplinaFelipe.php
@@ -8,7 +8,7 @@ include "config.php";
 
 $id = mysql_connect ( $host, $login_db, $senha_db );
 $con = mysql_select_db ( $database, $id );
-$login = $_GET ['login'];
+$login = $_COOKIE["login"];
 
 $sql = "SELECT p.nome, l.surveyls_title, d.disciplina_id  FROM sad_professor_disciplina as d, professores p, lime_surveys_languagesettings as l WHERE p.login = d.login and l.surveyls_survey_id         = d.disciplina_id and d.login = '$login'";
 $res = mysql_query ( $sql, $id );
