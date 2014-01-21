ec6d45095c6459ec0b8647350e48ef2a3f8fa6e2
Isabelly <isabelly.cavalcante@ccc.ufcg.edu.br>
2014-01-21 19:03:44 -0300

    iframe tentativa 7

--- sistema_lime/acessodb/imprimeGraficoDisciplinaFelipe.php
+++ sistema_lime/acessodb/imprimeGraficoDisciplinaFelipe.php
@@ -4,10 +4,6 @@
 <link href="../menu_assets/styles2.css" rel="stylesheet" type="text/css">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 
-<script src="http://d3js.org/d3.v3.min.js"></script>
-<script src="RadarChart.js"></script>
-
-
 <?php
 $disciplina_id = $_GET ['disciplina_id'];
 setcookie('disciplina_id',$disciplina_id);
