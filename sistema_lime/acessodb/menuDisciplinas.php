e88b1aba2e2a74a0a8fae1c4c3207c988fe72942
root <root@felipe-note.(none)>
2014-01-22 21:47:39 -0300

    Ajeitando tamanho do menu

--- sistema_lime/acessodb/menuDisciplinas.php
+++ sistema_lime/acessodb/menuDisciplinas.php
@@ -40,7 +40,7 @@ $res = mysql_query ( $sql, $id );
 			<iframe
 				name="disciplinas"
 				width="1320"
-				height="640"
+				height="670"
 				src='disciplinas.php'
 				frameborder="yes"
 				scrolling="yes">
