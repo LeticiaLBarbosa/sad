237d2e0fb9a96320ba2b46ce25867b27ac805e06
PET-Computacao <petcomputacao.ufcg@gmail.com>
2014-01-23 11:42:14 -0200

    Colocando texto inicial no menu

--- sistema_lime/acessodb/menuQuesitos.php
+++ sistema_lime/acessodb/menuQuesitos.php
@@ -10,6 +10,9 @@
                 <ul>
 <?php
 
+echo "Este gráfico, chamado de Radar Plot, é muito usado para representar observações multivariadas que envolvem diversas variáveis, onde cada "estrela" (formada pela ligação entre os pontos que determinam os valores das variáveis do gráfico para cada observação) representa uma observação. No caso dos dados da avaliação, a estrela de cor laranja representa a média do professor em cada quesito avaliado de uma dada disciplina ministrada por ele no semestre corrente; já a estrela de cor azul representa as maiores médias de cada quesito para todas as disciplinas oferecidas pelo departamento no semestre corrente; e a estrela de cor ... representa as menores médias de cada quesito para todas as disciplinas oferecidas pelo departamento no semestre corrente. Dessa forma é possível comparar o desempenho do professor em uma disciplina em relação aos resultados obtidos na avaliação docente do departamento no semestre corrente.
+
+Os quesitos usados para avaliar cada disciplina estão listados abaixo e para obter mais detalhes basta clicar no identificador de cada quesito:";
 
 for ($i = 0; $i < 15; $i++) {
    
