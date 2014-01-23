9456a770dcb1ed8e14f26f075b7b08d82c450344
PET-Computacao <petcomputacao.ufcg@gmail.com>
2014-01-23 02:05:15 -0200

    Update imprimeQuesito.php

--- sistema_lime/acessodb/imprimeQuesito.php
+++ sistema_lime/acessodb/imprimeQuesito.php
@@ -1,3 +1,27 @@
+<html>
+<head>
+<link href="../menu_assets/styles2.css" rel="stylesheet" type="text/css">
+   <meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
+</head>
+
+<body>
+
+   <div id="cssmenu">
+                <ul>
+<?php
+
+
+for ($i = 0; $i < 15; $i++) {
+   
+   $quesito = "Q".$i;
+
+echo "<li><a href=imprimeQuesito.php?quesito=$i>$quesito</a></li>";}
+
+?>
+</ul>
+
+        </div>
+
 <?php
 
 //	function imprimeQuesito($quesito){
@@ -226,3 +250,6 @@
 
 ?>
 
+</body>
+
+</html>
