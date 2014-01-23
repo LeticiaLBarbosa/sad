4f3f039da86105eee1d6912ba0a51dc90b4eae17
PET-Computacao <petcomputacao.ufcg@gmail.com>
2014-01-23 02:40:24 -0200

    Update imprimeQuesito.php

--- sistema_lime/acessodb/imprimeQuesito.php
+++ sistema_lime/acessodb/imprimeQuesito.php
@@ -133,7 +133,7 @@ echo "<li><a href=imprimeQuesito.php?quesito=$i>$quesito</a></li>";}
 		        if ($i == $quesito) {
 		        	echo "<br>";
 			        echo '<img src="geraGraficoFelipe.php?value1=' . $valorA1 . '&value2=' . $valorA2 . '&value3=' . $valorA3 . '&value4=' . $valorA4 . '&value5=' . $valorA5 . '" align="left" >';
-			        echo "<br>";
+			        echo "<p>";
 			        echo "<br>";
 			        echo "<br>";	
 				 
