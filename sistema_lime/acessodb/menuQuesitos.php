f2f2888968235f578523b0357d3378ff44d5030d
PET-Computacao <petcomputacao.ufcg@gmail.com>
2014-01-23 11:51:12 -0200

    Colocando texto inicial no menu 4

--- sistema_lime/acessodb/menuQuesitos.php
+++ sistema_lime/acessodb/menuQuesitos.php
@@ -1,6 +1,7 @@
 <html>
 <head>
-<link href="../menu_assets/styles2.css" rel="stylesheet" type="text/css">
+<!--<link href="../menu_assets/styles2.css" rel="stylesheet" type="text/css">
+-->
    <meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
 </head>
 
@@ -8,7 +9,9 @@
 
 <br>
 <p>
-Este gráfico, chamado de Radar Plot, é muito usado para representar observações multivariadas que envolvem diversas variáveis, onde cada "estrela" (formada pela ligação entre os pontos que determinam os valores das variáveis do gráfico para cada observação) representa uma observação. No caso dos dados da avaliação, a estrela de cor laranja representa a média do professor em cada quesito avaliado de uma dada disciplina ministrada por ele no semestre corrente; já a estrela de cor azul representa as maiores médias de cada quesito para todas as disciplinas oferecidas pelo departamento no semestre corrente; e a estrela de cor ... representa as menores médias de cada quesito para todas as disciplinas oferecidas pelo departamento no semestre corrente. Dessa forma é possível comparar o desempenho do professor em uma disciplina em relação aos resultados obtidos na avaliação docente do departamento no semestre corrente.
+Este gráfico, chamado de Radar Plot, é muito usado para representar observações multivariadas que envolvem diversas variáveis, onde cada "estrela" (formada pela ligação entre os pontos que determinam os valores das variáveis do gráfico para cada observação) representa uma observação.
+No caso dos dados da avaliação, a estrela de cor laranja representa a média do professor em cada quesito avaliado de uma dada disciplina ministrada por ele no semestre corrente; já a estrela de cor azul representa as maiores médias de cada quesito para todas as disciplinas oferecidas pelo departamento no semestre corrente; e a estrela de cor verde representa as menores médias de cada quesito para todas as disciplinas oferecidas pelo departamento no semestre corrente. Dessa forma é possível comparar o desempenho do professor em uma disciplina em relação aos resultados obtidos na avaliação docente do departamento no semestre corrente.
+<br>
 Os quesitos usados para avaliar cada disciplina estão listados abaixo e para obter mais detalhes basta clicar no identificador de cada quesito:
 </p>
 
