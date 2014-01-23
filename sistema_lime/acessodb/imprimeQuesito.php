e5c830ee9921d8473dd6cc542b848dce13b38174
PET-Computacao <petcomputacao.ufcg@gmail.com>
2014-01-23 01:47:40 -0200

    Update imprimeQuesito.php

--- sistema_lime/acessodb/imprimeQuesito.php
+++ sistema_lime/acessodb/imprimeQuesito.php
@@ -5,6 +5,7 @@
 		$quesito = $_GET['quesito'];
 		echo $quesito;
 		$disciplina_id = $_GET ['disciplina_id'];
+		echo $disciplina_id;
 		setcookie('disciplina_id',$disciplina_id);
 	
 		include "config.php";
