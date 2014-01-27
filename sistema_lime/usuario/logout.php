559fd3a62f32c57bdfcc8507e0ec6c0abf94f9e8
Felipe Sales <sales.felipeb@gmail.com>
2014-01-27 11:26:48 -0300

    ajeitando bug

--- sistema_lime/usuario/logout.php
+++ sistema_lime/usuario/logout.php
@@ -2,14 +2,13 @@
 
 ///	session_start();
 	
-	setcookie("login","",time()-3600);
-	setcookie("senha","",time()-3600);
-
+	
 	//unset($_SESSION['loginSession']);
 	//unset($_SESSION['senhaSession']);
 	
-	//unset($_COOKIE['login']);
-	//unset($_COOKIE['disciplina_id']);
+	unset($_COOKIE['login']);
+	unset($_COOKIE['disciplina_id']);
+	unset($_COOKIE['senha']);
 	//session_destroy();
 
 	header("Location: ../index.html");
