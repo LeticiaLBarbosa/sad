f82f9fda22b6e327ed112b98de7e5a5bf540e4d3
Isabelly <isabelly.cavalcante@ccc.ufcg.edu.br>
2014-01-28 17:28:54 -0300

    iframe radar

--- sistema_lime/acessodb/menuQuesitos.php
+++ sistema_lime/acessodb/menuQuesitos.php
@@ -1,8 +1,6 @@
 <html>
 <head>
-<!--<link href="../menu_assets/styles2.css" rel="stylesheet" type="text/css">
--->
-   <meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
 </head>
 
 <body>
@@ -10,32 +8,23 @@
 <br>
 <p align="justify">
 Este gráfico, chamado de Radar Plot, é muito usado para representar observações multivariadas que envolvem diversas variáveis, onde cada "estrela" (formada pela ligação entre os pontos que determinam os valores das variáveis do gráfico para cada observação) representa uma observação.
+<br>
 No caso dos dados da avaliação, a estrela de cor laranja representa a média do professor em cada quesito avaliado de uma dada disciplina ministrada por ele no semestre corrente; já a estrela de cor azul representa as maiores médias de cada quesito para todas as disciplinas oferecidas pelo departamento no semestre corrente; e a estrela de cor verde representa as menores médias de cada quesito para todas as disciplinas oferecidas pelo departamento no semestre corrente. Dessa forma é possível comparar o desempenho do professor em uma disciplina em relação aos resultados obtidos na avaliação docente do departamento no semestre corrente.
 <br>
 <br>
 Os quesitos usados para avaliar cada disciplina estão listados abaixo e para obter mais detalhes basta clicar no identificador de cada quesito:
 </p>
-
 <br>
 
-                
+   
 <?php
 
-
-
 for ($i = 0; $i < 15; $i++) {
-   
-   $quesito = "Q".($i + 1);
-
-echo "<a href=imprimeQuesito.php?quesito=$i>$quesito</a> &nbsp; &nbsp;" ;
-
+	$quesito = "Q".($i + 1);
+	echo "<a href=imprimeQuesito.php?quesito=$i>$quesito</a> &nbsp; &nbsp;" ;
 }
 
 ?>
-
-
-
-
 
 </body>
 
