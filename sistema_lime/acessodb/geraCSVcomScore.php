fcecaecfbe0ad0040c2d45e697ed1c515b38a864
Felipe Sales <sales.felipeb@gmail.com>
2014-01-30 15:46:27 -0300

    Voltando ao normal

---
+++ sistema_lime/acessodb/geraCSVcomScore.php
@@ -0,0 +1,41 @@
+<?php
+
+// Esse arquivo vai gerar o arquivo csv contendo os scores de cada disciplina por quesito avaliado, usando o csvFunction.php
+
+include 'config.php';
+include 'csvFunction.php';
+
+$fileCSV = fopen("data.csv", "w", 0);
+
+$id  = mysql_connect($host, $login_db, $senha_db);
+$con = mysql_select_db($database, $id);
+
+$sql = "SELECT p.nome, pd.disciplina_id, d.surveyls_title 
+FROM professores p, `sad_professor_disciplina` pd, lime_surveys_languagesettings d
+where p.login = pd.login and pd.disciplina_id = d.surveyls_survey_id";
+
+$res = mysql_query($sql, $id);
+
+fwrite($fileCSV, ("DisciplinaID,Q1,Q2,Q3,Q4,Q5,Q6,Q7,Q8,Q9,Q10,Q11,Q12,Q13\n"));
+
+while ($row = mysql_fetch_array($res)) {
+    
+    $linhaPronta = "";
+    
+    $linhaPronta .= mediasPorQuesito($row["disciplina_id"]);
+    
+    fwrite($fileCSV, ($linhaPronta . "\n"));
+    
+    echo $linhaPronta . "<br>";
+}
+
+fwrite($fileCSV, (geraMelhor() . "\n"));
+echo geraMelhor() . "<br>";
+
+
+fwrite($fileCSV, (geraPior() . "\n"));
+echo geraPior() . "<br>";
+
+fclose($fileCSV);
+
+?>
