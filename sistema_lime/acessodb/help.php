f24a7922a6d6eaa79a1a9f9ad79e7bc3111efea1
Isabelly <isabelly.cavalcante@ccc.ufcg.edu.br>
2014-02-01 23:01:50 -0300

    css 4

--- sistema_lime/acessodb/help.php
+++ sistema_lime/acessodb/help.php
@@ -9,7 +9,7 @@ verificaLogin();
 
 <html>
 <head>
-	<link href="../menu_assets/styles3.css" rel="stylesheet" type="text/css">
+	<link href="../menu_assets/styles2.css" rel="stylesheet" type="text/css">
 	<meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
 
 </head>
