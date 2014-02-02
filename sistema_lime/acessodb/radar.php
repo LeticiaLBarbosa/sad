5237af0817e642d5bcb71398f2ebfe1e1b9e2eed
PET-Computacao <petcomputacao.ufcg@gmail.com>
2014-02-01 22:32:43 -0200

    Update radar.php

--- sistema_lime/acessodb/radar.php
+++ sistema_lime/acessodb/radar.php
@@ -6,28 +6,6 @@
         <script src="RadarChart.js"></script>
   </head>
   
-<?php
-	
-	session_start();
-	
-	$disciplina_id = $_GET ['disciplina_id'];
-	
-	setcookie('disciplina_id',$disciplina_id);
-
-	
-	include "config.php";
-
-	$id = mysql_connect ( $host, $login_db, $senha_db );
-	$con = mysql_select_db ( $database, $id );
-	$login = $_SESSION['login'];
-
-	$sql = "SELECT p.nome, l.surveyls_title, d.disciplina_id  FROM sad_professor_disciplina as d, professores p, lime_surveys_languagesettings as l WHERE p.login = d.login and l.surveyls_survey_id         = d.disciplina_id and d.login = '$login'";
-	$res = mysql_query ( $sql, $id );
-	
-	echo $disciplina_id;
-
-
-?>
   <br>
   <h3><center>Disciplina</center></h3>
   
