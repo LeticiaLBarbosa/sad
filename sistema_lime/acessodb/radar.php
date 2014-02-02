c698f8f39bd1c4ff4f09cc010e7b00c188d72ec9
PET-Computacao <petcomputacao.ufcg@gmail.com>
2014-02-01 23:04:25 -0200

    Update radar.php

--- sistema_lime/acessodb/radar.php
+++ sistema_lime/acessodb/radar.php
@@ -7,14 +7,47 @@
   </head>
   
   <br>
-  <h3><center>Disciplina</center></h3>
+  
   
   <body>
+  
+    <h3><center><?php   
+        
+    include "config.php";        
+        
+    session_start();
+    
+    $disciplina_idAtual = $_COOKIE['disciplina_id'];
+    
+    $id    = mysql_connect($host, $login_db, $senha_db);
+    $con   = mysql_select_db($database, $id);
+    $login = $_SESSION['login'];
+    
+    $sql = "SELECT p.nome, l.surveyls_title, d.disciplina_id  FROM sad_professor_disciplina as d, professores p, lime_surveys_languagesettings as l WHERE p.login = d.login and l.surveyls_survey_id         = d.disciplina_id and d.login = '$login'";
+    $res = mysql_query($sql, $id);
+    
+    while ($row = mysql_fetch_array($res)) {
+				$disciplina    = $row["surveyls_title"];
+				$disciplina_id = $row["disciplina_id"];
+				
+				if ($disciplina_id == $disciplina_idAtual){
+				  echo $disciplina;
+				  break;
+				  
+				}
+				
+				//echo "<li><a href=imprimeDisciplina.php?disciplina_id=$disciplina_id>$disciplina</a></li>";
+		}
+		
+    
+    ?></center></h3>
+    
     <div id="body">
-          <div id="chart"></div>
+          <div id="chart"><script type="text/javascript" src="script.js"></script></div>
+          
     </div>
         
-    <script type="text/javascript" src="script.js"></script>
+    
         
   </body>
 </html>
