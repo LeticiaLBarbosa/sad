5693f8310f05765d83806c54033960dbced407e0
PET-Computacao <petcomputacao.ufcg@gmail.com>
2014-02-03 10:14:52 -0200

    Update ranking.js

--- sistema_lime/ranking/ranking.js
+++ sistema_lime/ranking/ranking.js
@@ -57,7 +57,7 @@ questao_avaliada = p; //livia: guarda o identificador da questao escolhida
             .attr("text-anchor", "center")
             .attr("font-size", "12px")
             .attr("font-weight", "bold")
-            .text("Esta eh a unica disciplina avaliada na questao " + questao_avaliada); //
+            .text("Esta eh a unica disciplina avaliada na questao " +p+ questao_avaliada); //
     }else{
         plot_ranges_ranking(svg, line_per, h1);
         plot_bars_ranking(svg, line_per, h1);            
