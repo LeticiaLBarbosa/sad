8af282ae7d27172ed382b302cf636078fab61df7
Felipe Sales <sales.felipeb@gmail.com>
2014-02-07 16:57:07 -0300

    Dados reais

--- sistema_lime/acessodb/config.php
+++ sistema_lime/acessodb/config.php
@@ -3,7 +3,7 @@
 	// Arquivo contendo as configuracoes necessarias para acessar o banco de dados
 
 	$host		=	'localhost'; //endereço do seu servidor MySQL
-	$database	=	'meu_banco_db'; //o database que conterá sua tabela, muitas vezes seu próprio login
+	$database	=	'sad'; //o database que conterá sua tabela, muitas vezes seu próprio login
 	$tabela		=	'professores'; //o nome de sua tabela
 	$login_db	=	'root'; //login usado no MySQL
 	$senha_db	=	'm0n1c4'; //senha usado no MySQL
