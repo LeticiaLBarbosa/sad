545c48aa6810c7959d315d4fd54e2070bd4ae065
Isabelly <isabelly.cavalcante@ccc.ufcg.edu.br>
2014-02-08 16:00:29 -0300

    alterando forma de imprimir os titulos 5

--- sistema_lime/acessodb/geraQuesito.php
+++ sistema_lime/acessodb/geraQuesito.php
@@ -1,6 +1,16 @@
 <?php
 
+include "config.php";
+include "geraMediaPorQuesito.php";
+include "csvFunction.php";
+
+// Iniciando conexao ao BD e gerando variaveis essenciais
+$id  = mysql_connect($host, $login_db, $senha_db);
+$con = mysql_select_db($database, $id);
+
+
 $TituloRespostas = array();
+$Respostas = array[][];
 
 $TituloRespostas[0] = "Os pré-requisitos assumidos pela disciplina foram adequados?";
 $TituloRespostas[1] = "O programa da disciplina está de acordo com a ementa da mesma?";
@@ -23,6 +33,33 @@ for ($i = 0; $i < 15; $i ++){
 	echo "<br>";
 }
 
+$Respostas[0][0] = "Totalmente";
+$Respostas[0][1] = "Parcialmente, mas o professor ministrou o conteúdo que faltava"; 
+$Respostas[0][2] = "Parcialmente";
+$Respostas[0][3] = "Pouco";
+$Respostas[0][4] = "Absolutamente, tive que estudar por conta própria o conteúdo que deveria ter sido dado em disciplinas anteriores";
+$Respostas[1][0] = "Totalmente";
+$Respostas[1][1] = "Quase totalmente";
+$Respostas[1][2] = "Neutro";
+$Respostas[1][3] = "Muito pouco";
+$Respostas[1][4] = "Em desacordo";
+$Respostas[2][0] = "Muito bom, facilitou o aprendizado";
+$Respostas[2][1] = "Bom";
+$Respostas[2][2] = "Não teve impacto";
+$Respostas[2][3] = "Ruim";
+$Respostas[2][4] = "Muito ruim";
+$Respostas[3][0] = "Muito alto, continha tudo que era necessário, básico e avançado";
+$Respostas[3][1] = "Acima da média";
+$Respostas[3][2] = "Mediano: supriu o básico";
+$Respostas[3][3] = "Ruim: precisei procurar material adicional";
+$Respostas[3][4] = "Muito ruim, substituí completamente por outro material";
+$Respostas[4][0] = "Muito alto";
+$Respostas[4][1] = "Alto";
+$Respostas[4][2] = "Médio";
+$Respostas[4][3] = "Regular, com certa insegurança";
+$Respostas[4][4] = "Fraco, com total insegurança";
+
+echo $Respostas[4][4];
 
 
 
