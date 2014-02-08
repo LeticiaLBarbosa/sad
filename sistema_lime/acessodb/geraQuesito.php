72562ee2515716eb1794ea97e73fae619ee1cdfd
Isabelly <isabelly.cavalcante@ccc.ufcg.edu.br>
2014-02-08 14:25:12 -0300

    criando função GeraQuesito

---
+++ sistema_lime/acessodb/geraQuesito.php
@@ -0,0 +1,39 @@
+<?php
+
+$TituloRespostas = array();
+
+$TituloRespostas[0] = "Os pré-requisitos assumidos pela disciplina foram adequados?";
+$TituloRespostas[1] = "O programa da disciplina está de acordo com a ementa da mesma?";
+$TituloRespostas[2] = "A metodologia usada pelo professor (recursos didáticos, atividades dentro e fora de sala de aula) teve qual impacto no aprendizado?";
+$TituloRespostas[3] = "A bibliografia apresentada teve qual impacto no seu aprendizado?";
+$TituloRespostas[4] = "Qual é o nível de domínio do assunto pelo professor?";
+$TituloRespostas[5] = "O método de avaliação foi apropriado para o conteúdo da disciplina?";
+$TituloRespostas[6] = "Na sua opinião, quanto do material ministrado na disciplina você aprendeu bem?";
+$TituloRespostas[7] = "Como você avalia a pontualidade do professor?";
+$TituloRespostas[8] = "Como você avalia a assiduidade do professor?";
+$TituloRespostas[9] = "Para as aulas que o professor faltou (no caso, faltas não previstas no cronograma da disciplina), houve reposição em outros horários?";
+$TituloRespostas[10] = 'Como o professor equilibrou teoria/prática na disciplina (em disciplinas de laboratório, considere como "teoria" a orientação do professor para os exercícios)?';
+$TituloRespostas[11] = "A comunicação da turma com o professor tem qual nível de qualidade?";
+$TituloRespostas[12] = "O professor demonstra preocupação com o aprendizado dos alunos?";
+$TituloRespostas[13] = "Como você avalia a infra-estrutura da sala de aula (ou laboratório)?";
+$TituloRespostas[14] = "Quais os tipos de problema mais sérios que você detectou durante esta disciplina (checkboxes, podendo escolher mais de um)?";
+
+for ($i = 0; $i < 15; $i ++){
+	echo $TitutoRespostas[i];
+}
+
+echo "passou";
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
