d95048dab1b438b3f456821ddc922c6878c35ac4
Isabelly <isabelly.cavalcante@ccc.ufcg.edu.br>
2014-02-08 14:31:44 -0300

    criando funcao GeraQuesito 2

--- sistema_lime/acessodb/geraQuesito.php
+++ sistema_lime/acessodb/geraQuesito.php
@@ -19,9 +19,10 @@ $TituloRespostas[13] = "Como você avalia a infra-estrutura da sala de aula (ou 
 $TituloRespostas[14] = "Quais os tipos de problema mais sérios que você detectou durante esta disciplina (checkboxes, podendo escolher mais de um)?";
 
 for ($i = 0; $i < 15; $i ++){
+	echo $i;
 	echo $TitutoRespostas[i];
 }
-
+echo $TituloRespostas[0];
 echo "passou";
 
 
