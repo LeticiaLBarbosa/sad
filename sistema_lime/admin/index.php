6b76d3e5106bac5df12a220c3a265e49f6a1b3fd
Felipe Sales <sales.felipeb@gmail.com>
2014-02-11 16:44:52 -0300

    ranking modulo de urgencia

--- sistema_lime/admin/index.php
+++ sistema_lime/admin/index.php
@@ -22,47 +22,48 @@ d3.csv("ranking.csv",function(data){
     dados_ranking = data;
 
 	//var nome = getCookie('disciplina_id'); //livia: aqui vem a leitura do cookie
-	var nome = 'desvioPositivo';
+	//var nome = 'desvioPositivo';
 	var p = "<?php echo "Q". $_GET['quesito']; 
-	//echo "Q1";?>"; //livia: aqui vem a leitura do cookie
 
-	plot_bar_disciplina_ranking(nome, p);
+	plot_bar_disciplina_ranking(p);
 
 });
 
-function plot_bar_disciplina_ranking(nome, p){
-	disciplina = nome;
-    	questao_avaliada = p; //livia: guarda o identificador da questao escolhida
+
+//livia: a funcao getRanking foi removida pois nao era usada
+
+function plot_bar_disciplina_ranking(p){
+questao_avaliada = p; //livia: guarda o identificador da questao escolhida
+
     var h1 = 60;
-    var margin = {top: 30, right: 120, bottom: 40, left: 40},
-            width = 750 - margin.left - margin.right,
-            height = 400 - margin.top - margin.bottom;
+    var margin = {top: 30, right: 120, bottom: 40, left: 60},
+            width = 950 - margin.left - margin.right,
+            height = 600 - margin.top - margin.bottom;
     d3.select("#infos").select("svg").remove();
     var svg = d3.select("#infos");
     
     svg = d3.select("#infos").append("svg")
-        .attr("width", 600)
-        .attr("height", 400);	
+        .attr("width", 800)
+        .attr("height", 600);	
     
     var val_per = dados_ranking.filter(function(d){ return d.questao == questao_avaliada});
 
-    var line_per =  [{'x' : d3.min(val_per,function(d){return parseFloat(d.media);}) , 'y' : h1},
+     var line_per =  [{'x' : d3.min(val_per,function(d){return parseFloat(d.media);}) , 'y' : h1},
                          {'x':(d3.max(val_per,function(d){return parseFloat(d.media);})), 'y' : h1}];
 
     if (val_per.length<2) {
         // Imprime mensagem
         svg.append("text")
             .attr("y", h1-30)
-            .attr("x", 350)
+            .attr("x", 550)
             .attr("text-anchor", "center")
             .attr("font-size", "12px")
             .attr("font-weight", "bold")
-            .text(disciplina+ "Esta eh a unica disciplina avaliada na questao "); //
+            .text("Esta eh a unica disciplina avaliada na questao " + questao_avaliada); //
     }else{
         plot_ranges_ranking(svg, line_per, h1);
         plot_bars_ranking(svg, line_per, h1);            
         plot_disciplinas_ranking(svg, val_per, "blue",line_per[0], line_per[1],h1);
-
     };
 }
 
@@ -70,10 +71,11 @@ function plot_bar_disciplina_ranking(nome, p){
 function plot_ranges_ranking(svg, dados, y0){
     var valor1 = String(dados[0].x).replace(/\,/g,'');
     var valor2 = String(dados[1].x).replace(/\,/g,'');
+
     
     var x1 = d3.scale.linear()
           .domain([parseFloat(valor1), parseFloat(valor2)])
-          .range([120, 550]);    
+          .range([120, 750]);    
 
     var xAxis = d3.svg.axis()
             .scale(x1)
@@ -103,6 +105,7 @@ function plot_ranges_ranking(svg, dados, y0){
             .attr("x",x1(dados[1].x) + 10)
             .attr("y",(y0 + 8))
             .text(valor2);
+
 }
 
 /* Funcao para plotar uma barrinha*/
@@ -137,22 +140,20 @@ function plot_bars_ranking(svg, dados,y0){
     
     var x1 = d3.scale.linear()
           .domain([dados[0].x, dados[1].x])
-          .range([120, 550]);
+          .range([120, 750]);
     
     addLine_ranking(svg,x1(dados[0].x),x1(dados[1].x),y0,y0,"#E0E0E0");
 }
 
 
 function convert(nota,min,max){
-    return (((nota- min)/(max-min))*(550-120)) + 120; //livia: algum tipo de normalizacao?????
+    return (((nota- min)/(max-min))*(750-120)) + 120; //livia: algum tipo de normalizacao?????
 }
 
 function plot_disciplinas_ranking(svg, dados, cor, min, max, y0){
-	console.log(dados);
-    var inf = dados.filter(function(d) {return d.disciplina == disciplina});
-    var inf2 = dados.filter(function(d) {return d.disciplina == "desvioNegativo"});
-    console.log(inf);
-    console.log(inf2);
+console.log(dados);
+    var dp = "DP";
+    var dn = "DN";
     
     function mousemove(nota, disciplina) { 
         svg.append("text")
@@ -176,22 +177,6 @@ function plot_disciplinas_ranking(svg, dados, cor, min, max, y0){
         .text(disciplina + ": " + nota);
     } 
 
-    // Adiciona o texto do ranking
-    svg.append("text")
-        .attr("x", function(d){ return convert(inf[0].media,min.x,max.x) - 15;})
-        .attr("y",(y0 - 15)) // Altura de onde o texto vai aparecer
-        .attr("text-anchor", "middle")
-        .attr("font-size", "12px")
-        .text(inf[0].posicao + "º colocado");
-    
-    // Adiciona o texto da nota do aluno selecionado 
-    svg.append("text")
-        .attr("x", function(d){ return convert(inf[0].media,min.x,max.x) ;})
-        .attr("y",(y0 + 25)) // Altura de onde o texto vai aparecer
-        .attr("text-anchor", "middle")
-        .attr("font-weight", "bold")
-        .text(inf[0].media);
-
     var g = svg.append("g");
 
     // Adiciona as linhas correspondente as notas na questao escolhida para cada disciplina
@@ -204,78 +189,21 @@ function plot_disciplinas_ranking(svg, dados, cor, min, max, y0){
                     .attr("y2",y0+12) // Altura inferior da linha
                     .attr("class","linha_disciplina")
                     .transition().duration(duration) // Transicao
-                    .style("stroke","#0000a1") // Cor da linha
+                    .style("stroke",function(d){ 
+				if( d.disciplina == dp || d.disciplina == dn){
+					return "red";
+				}else{
+					return "#0000a1";
+			}}) // Cor da linha
                     .attr("stroke-width",5)    // Largura da linha
                     .attr("text",function(d){return d.disciplina;});
 
-    g.selectAll("line").on("mouseout", function(d){mouseout(d.media, d.disciplina);}) 
+    g.selectAll("line").data(dados).on("mouseout", function(d){mouseout(d.media, d.disciplina);}) 
                        .on("mousemove", function(d){mousemove(d.media, d.disciplina);})
                        .on("click", function(d) {console.log(d.disciplina + "  " + d.media);});
 
 
-    // Adiciona a linha correspondente a media do aluno escolhido
-    svg.append("line")
-            .attr("x1", function(d){ return convert(inf[0].media,min.x,max.x);}) // X inicial da linha
-            .attr("x2", function(d){ return convert(inf[0].media,min.x,max.x);}) // X final da linha 
-            .attr("y1",y0-12) // Y inicial da linha
-            .attr("y2",y0+12) // Y final da linha
-            .attr("class","linha_disciplina")
-            .transition().duration(duration)  // Transicao
-            .style("stroke","red") // Cor da linha
-            .attr("stroke-width",5)    // Largura da linha
-            .attr("text",function(d){return d.disciplina;});
-    
-    svg.selectAll("line").on("mouseover", function(d){mouseover();}) 
-                       .on("mouseout", function(d){mouseout(d.media, d.disciplina);}) 
-                    .on("mousemove", function(d){mousemove(d.media, d.disciplina);})
-                    .on("click", function(d) {console.log(d.disciplina + "  " + d.media);});
-                    
-                    
-    // Adiciona a linha correspondente a media do aluno escolhido
-    svg.append("line")
-            .attr("x1", function(d){ return convert(inf2[0].media,min.x,max.x);}) // X inicial da linha
-            .attr("x2", function(d){ return convert(inf2[0].media,min.x,max.x);}) // X final da linha 
-            .attr("y1",y0-12) // Y inicial da linha
-            .attr("y2",y0+12) // Y final da linha
-            .attr("class","linha_disciplina")
-            .transition().duration(duration)  // Transicao
-            .style("stroke","red") // Cor da linha
-            .attr("stroke-width",5)    // Largura da linha
-            .attr("text",function(d){return d.disciplina;});
-    
-    svg.selectAll("line").on("mouseover", function(d){mouseover();}) 
-                       .on("mouseout", function(d){mouseout(d.media, d.disciplina);}) 
-                    .on("mousemove", function(d){mousemove(d.media, d.disciplina);})
-                    .on("click", function(d) {console.log(d.disciplina + "  " + d.media);});
-
-
-
 }
-
-function getCookie(c_name){
-	var c_value = document.cookie;
-	var c_start = c_value.indexOf(" " + c_name + "=");
-	if (c_start == -1)
-	  {
-	  c_start = c_value.indexOf(c_name + "=");
-	  }
-	if (c_start == -1)
-	  {
-	  c_value = null;
-	  }
-	else
-	  {
-	  c_start = c_value.indexOf("=", c_start) + 1;
-	  var c_end = c_value.indexOf(";", c_start);
-	  if (c_end == -1)
-	  {
-	c_end = c_value.length;
-	}
-	c_value = unescape(c_value.substring(c_start,c_end));
-	}
-	return c_value;
-}
-
 
 </script>
   
